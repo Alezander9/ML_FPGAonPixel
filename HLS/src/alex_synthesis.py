4571b2af4af19dea0d1883104180cc156b6560ed
Alezander9 <Alexander.j.yue@gmail.com>
2024-07-25 12:22:48 -0700

    updated performance metrics post synthesis

--- HLS/src/alex_synthesis.py
+++ HLS/src/alex_synthesis.py
@@ -1,9 +1,17 @@
 # PATH OF MODEL TO SYNTHESIZE
-tslices = 4
-wbbits = 10
-abits = 15
-ibits = 2
-skl_model_path = f'../../software/ALEX/{tslices}_slice_L2_S24_{wbbits}_{abits}_best.h5'
+HYPERPARAMETERS = {
+    "NUM_TIME_SLICES": 4,
+    "QUANTIZATION_BITS": [4, 6, 2], # weights&biases, activation, integer
+    "OUTPUT": "SINGLE",
+}
+NUM_TIME_SLICES = HYPERPARAMETERS["NUM_TIME_SLICES"]
+WEIGHTS_BITS = HYPERPARAMETERS["QUANTIZATION_BITS"][0]
+BIAS_BITS = HYPERPARAMETERS["QUANTIZATION_BITS"][0]
+ACTIVATION_BITS = HYPERPARAMETERS["QUANTIZATION_BITS"][1]
+INTEGER_BITS = HYPERPARAMETERS["QUANTIZATION_BITS"][2]
+
+skl_model_path = f'../../software/ALEX/{NUM_TIME_SLICES}_slice_L2_S24_{WEIGHTS_BITS}_{ACTIVATION_BITS}_best.h5'
+
 
 # IMPORTS
 import hls4ml
@@ -59,9 +67,9 @@ hls_config['Model']['ReuseFactor'] = 1 # MOST RELAVANT PARAMETER
 for Layer in hls_config['LayerName'].keys():
     print(Layer)
     hls_config['LayerName'][Layer]['Strategy'] = 'Latency'
-    hls_config['LayerName'][Layer]['Precision']['weight'] = f'ap_fixed<{wbbits},{ibits}>'
-    hls_config['LayerName'][Layer]['Precision']['bias'] = f'ap_fixed<{wbbits},{ibits}>'
-    hls_config['LayerName'][Layer]['Precision']['result'] = f'ap_fixed<{abits},{ibits}>'
+    hls_config['LayerName'][Layer]['Precision']['weight'] = f'ap_fixed<{WEIGHTS_BITS},{INTEGER_BITS}>'
+    hls_config['LayerName'][Layer]['Precision']['bias'] = f'ap_fixed<{BIAS_BITS},{INTEGER_BITS}>'
+    hls_config['LayerName'][Layer]['Precision']['result'] = f'ap_fixed<{ACTIVATION_BITS},{INTEGER_BITS}>'
     hls_config['LayerName'][Layer]['Trace'] = True
 
 cfg = hls4ml.converters.create_config(backend='Vitis')
@@ -165,34 +173,35 @@ def read_data_from_file(file_path):
         return data
     
 # TEST MODEL
-def testModel():
-    input_data_file_path = './4_slice_test_inputs.dat'
-    output_data_file_path = './single_test_outputs.dat'
 
-    data = read_data_from_file(input_data_file_path)
-    y_sum_test = read_data_from_file(output_data_file_path)
+def test_model(data, model, hyperparams):
+    OUTPUT = hyperparams["OUTPUT"]
+    
+    input_test_data_combined = data["input_test_data_combined"]
+    target_test_data_coded = data["target_test_data_coded"]
 
-    print('###################### TEST DATASET HAS BEEN LOADED  ######################')
+    
+    # Test the model at threshold 0.5
+    predictions = model.predict(input_test_data_combined)
+    print(predictions[:10, :])
+    predictions_prob = predictions[:,0]
+    predictions_labels = (predictions_prob >= 0.5).astype(int).flatten()
 
-    X_sum_test = data[:, :104]  # All rows, first 104 columns
-    y0_test = data[:, 104]  # All rows, last column
-    X_sum_test = X_sum_test.reshape(-1, 8, 13)  #
-    y_sum_test = y_sum_test.reshape(-1, 1)
-    print("y_sum = ",  np.sum(y_sum_test))
-
-    # perfect_pred = model.predict([X_sum_test, y0_test])
-    pred = hls_model.predict([np.ascontiguousarray(X_sum_test), np.ascontiguousarray(y0_test)])
-    hls_model.compile()
-
-    thresholds = np.linspace(0, 1, 1000)
+    # Test the model at different thresholds
+    thresholds = np.linspace(0.0, 1.0, 1000)
     signal_efficiencies = []
     background_rejections = []
+    max_sum_se = 0
+    max_sum_br = 0
 
     for threshold in thresholds:
-        predicted_class = (pred > threshold).astype(int)
-
+        # predicted_class = ((predictions_prob[:, 0] + threshold > predictions_prob[:, 1]) & (predictions_prob[:, 0] + threshold > predictions_prob[:, 2])).astype(int)
+        predicted_class = (predictions_prob > threshold).astype(int)
         # Compute confusion matrix
-        cm = confusion_matrix(y_sum_test[:, 0], predicted_class)
+        if OUTPUT == "SINGLE":
+            cm = confusion_matrix(target_test_data_coded[:], predicted_class)
+        else:
+            cm = confusion_matrix(target_test_data_coded[:, 0], predicted_class)
 
         # Calculate signal efficiency and background rejection
         signal_efficiency = cm[1, 1] / np.sum(cm[1, :])
@@ -202,7 +211,46 @@ def testModel():
         signal_efficiencies.append(signal_efficiency)
         background_rejections.append(background_rejection)
 
-    # Plotting
+        # get maximum added score
+        if signal_efficiency + background_rejection > max_sum_se + max_sum_br:
+            max_sum_se = signal_efficiency
+            max_sum_br = background_rejection
+    
+    test_results = {
+        "predictions_prob": predictions_prob,
+        "predictions_labels": predictions_labels,
+        "thresholds": thresholds,
+        "signal_efficiencies": signal_efficiencies,
+        "background_rejections": background_rejections,
+        "max_sum_se": max_sum_se,
+        "max_sum_br": max_sum_br,
+    }
+
+    return test_results
+
+def ShowConfusionMatrix(data, test_results, hyperparams):
+    OUTPUT = hyperparams["OUTPUT"]
+    
+    target_test_data_coded = data["target_test_data_coded"]
+    predictions_labels = test_results["predictions_labels"]
+
+    if OUTPUT == "SINGLE":
+        cm = confusion_matrix(target_test_data_coded[:], predictions_labels)
+    else:
+        cm = confusion_matrix(target_test_data_coded[:, 0], predictions_labels)
+
+    plt.figure(figsize=(8, 6))
+    sns.heatmap(cm, annot=True, fmt='g', cmap='YlGnBu')
+    plt.xlabel('Predicted labels')
+    plt.ylabel('True labels')
+    plt.title('Confusion Matrix')
+    plt.show()
+
+def showMetricsByThreshold(test_results):
+    thresholds = test_results["thresholds"]
+    signal_efficiencies = test_results["signal_efficiencies"]
+    background_rejections = test_results["background_rejections"]
+
     plt.figure(figsize=(10, 6))
     plt.plot(thresholds, signal_efficiencies, label='Signal Efficiency')
     plt.plot(thresholds, background_rejections, label='Background Rejection')
@@ -213,6 +261,10 @@ def testModel():
     plt.grid(True)
     plt.show()
 
+def showEfficiencyVSRejection(test_results):
+    signal_efficiencies = test_results["signal_efficiencies"]
+    background_rejections = test_results["background_rejections"]
+
     plt.figure(figsize=(10, 6))
     plt.plot(signal_efficiencies, background_rejections, marker='o')
     plt.xlabel('Signal Efficiency')
@@ -221,23 +273,43 @@ def testModel():
     plt.grid(True)
     plt.show()
 
-    def find_closest(sorted_array, value):
-        sorted_array = np.array(sorted_array)
-        abs_diff = np.abs(sorted_array - value)
-        closest_index = np.argmin(abs_diff)
-        return closest_index
+def find_closest(sorted_array, value):
+    # Ensure the array is a NumPy array
+    sorted_array = np.array(sorted_array)
+    # Compute the absolute difference
+    abs_diff = np.abs(sorted_array - value)
+    # Find the index of the minimum difference
+    closest_index = np.argmin(abs_diff)
+    return closest_index
 
-    index_848 = find_closest(signal_efficiencies, 0.848)
-    index_933 = find_closest(signal_efficiencies, 0.933)
-    index_976 = find_closest(signal_efficiencies, 0.976)
-    index_995 = find_closest(signal_efficiencies, 0.995)
-    index_997 = find_closest(signal_efficiencies, 0.997)
+def getTargetMetrics(test_results):
+    signal_efficiencies = test_results["signal_efficiencies"]
+    background_rejections = test_results["background_rejections"]
 
-    print(f"Signal Efficiency:{signal_efficiencies[index_848]*100:.1f}%",f"Background Rejections:{background_rejections[index_848]*100:.1f}%")
-    print(f"Signal Efficiency:{signal_efficiencies[index_933]*100:.1f}%",f"Background Rejections:{background_rejections[index_933]*100:.1f}%")
-    print(f"Signal Efficiency:{signal_efficiencies[index_976]*100:.1f}%",f"Background Rejections:{background_rejections[index_976]*100:.1f}%")
-    print(f"Signal Efficiency:{signal_efficiencies[index_995]*100:.1f}%",f"Background Rejections:{background_rejections[index_995]*100:.1f}%")
-    print(f"Signal Efficiency:{signal_efficiencies[index_997]*100:.1f}%",f"Background Rejections:{background_rejections[index_997]*100:.1f}%")
+    target_efficiencies = [0.873, 0.90, 0.93, 0.96, 0.98, 0.99, 0.995, 0.999]
+    metrics = []
+    for target in target_efficiencies:
+        index = find_closest(signal_efficiencies, target)
+        metrics.append((signal_efficiencies[index], background_rejections[index]))
+        # print(f"Signal Efficiency: {signal_efficiencies[index]*100:.1f}%,",f"Background Rejections: {background_rejections[index]*100:.1f}%")
+    return metrics
 
-testModel()
+def displayPerformance(data, test_results, metrics, hyperparams):
+    ShowConfusionMatrix(data, test_results, hyperparams)
+    showMetricsByThreshold(test_results)
+    showEfficiencyVSRejection(test_results)
+    print(metrics)
 
+
+print('###################### LOADING TEST DATASET  ######################')
+input_data_file_path = './4_slice_test_inputs.dat'
+output_data_file_path = './single_test_outputs.dat'
+data = {
+    "input_test_data_combined": read_data_from_file(input_data_file_path),
+    "target_test_data_coded": read_data_from_file(output_data_file_path)
+}
+
+print('###################### DISPLAYING PERFORMANCE  ######################')
+test_results = test_model(data, model, HYPERPARAMETERS)
+metrics = getTargetMetrics(test_results)
+displayPerformance(data, test_results, metrics, HYPERPARAMETERS)
